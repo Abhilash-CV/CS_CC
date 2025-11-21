6f9042fc7b0cb009d9ae6afde35f6bf113251af1
Abhilash-CV <abhilashmayavi@gmail.com>
2025-11-21 10:19:26 +0530

    Update 7_Conflict_Resolution.py

--- pages/7_Conflict_Resolution.py
+++ pages/7_Conflict_Resolution.py
@@ -2,6 +2,9 @@ import streamlit as st
 if 'role' not in st.session_state or st.session_state['role'] != 'admin':
     st.error("Unauthorized access. Please login as Admin.")
     st.stop()
+if st.sidebar.button("🔓 Logout"):
+    st.session_state.role = None
+    st.rerun()
 st.title("⚔️ Conflict Resolution")
 
 st.write("Center: Loyola College")
