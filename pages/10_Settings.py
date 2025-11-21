f9fe4775ebf12508429277c4859c45eab8f285e3
Abhilash-CV <abhilashmayavi@gmail.com>
2025-11-21 10:21:24 +0530

    Update 10_Settings.py

--- pages/10_Settings.py
+++ pages/10_Settings.py
@@ -1,5 +1,10 @@
 import streamlit as st
-
+if 'role' not in st.session_state or st.session_state['role'] != 'admin':
+    st.error("Unauthorized access. Please login as Admin.")
+    st.stop()
+if st.sidebar.button("🔓 Logout"):
+    st.session_state.role = None
+    st.rerun()
 st.title("⚙️ Settings")
 
 st.number_input("Max Preferences Allowed", min_value=1, max_value=10, value=5)
