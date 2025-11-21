194840d549d78a10ed93091ab31f84aec9a38f92
Abhilash-CV <abhilashmayavi@gmail.com>
2025-11-21 09:33:20 +0530

    Update 2_Manage_Sessions.py

--- pages/2_Manage_Sessions.py
+++ pages/2_Manage_Sessions.py
@@ -1,4 +1,7 @@
 import streamlit as st
+if 'role' not in st.session_state or st.session_state['role'] != 'admin':
+    st.error("Unauthorized access. Please login as Admin.")
+    st.stop()
 st.title("🗓️ Manage Exam Sessions")
 
 st.text_input("Session Name")
